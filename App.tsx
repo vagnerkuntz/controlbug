ce6e32a5325f0d059df79566ca9f110a52bc99d5
vagnerkuntz <vaagner.kuntz@gmail.com>
2022-04-30 13:48:10 -0300

    build: add typescript in project

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>...</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
