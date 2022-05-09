6db99964c1a1d17c1324d99ecd74f4c44105ae0d
vagnerkuntz <vaagner.kuntz@gmail.com>
2022-05-09 11:27:59 -0300

    chore: add firebase and netinfo packages

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {View, Button, SafeAreaView, Text} from 'react-native';
+import {useNetInfo} from '@react-native-community/netinfo';
 
 const App: React.FC = () => {
+  const netInfo = useNetInfo();
+
   return (
     <SafeAreaView>
       <Text>...</Text>
+      <View>
+        <Button title="internet?" disabled={!netInfo.isInternetReachable} />
+      </View>
     </SafeAreaView>
   );
 };
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import database from '@react-native-firebase/database';
+
+database().setPersistenceEnabled(true);
+database().setPersistenceCacheSizeBytes(104857600); // 100MB
 
 AppRegistry.registerComponent(appName, () => App);
